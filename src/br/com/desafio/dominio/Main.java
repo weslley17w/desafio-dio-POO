dea233fb5621743f27ad8fe7c744ef7d833b3e05
Weslley Barros <weslley17w@gmail.com>
2022-03-07 15:37:11 -0300

    Add class Main

---
+++ src/br/com/desafio/dominio/Main.java
@@ -0,0 +1,28 @@
+package br.com.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("descrição");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("descrição");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("descrição");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(mentoria1);
+    }
+}
